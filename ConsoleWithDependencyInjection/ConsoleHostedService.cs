29bca4e0ebd47ecf081e4d41036ae9d671e52853
Milan Křepelka <milan.krepelka@assecosol.com>
2023-08-28 17:00:54 +0200

    Base console app with Console hosted service

---
+++ ConsoleWithDependencyInjection/ConsoleHostedService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWithDependencyInjection
+{
+    /// <summary>
+    /// Console running code as hosted service
+    /// </summary>
+    /// <see cref="https://learn.microsoft.com/en-us/aspnet/core/fundamentals/host/hosted-services?view=aspnetcore-7.0&tabs=visual-studio"/>
+    internal sealed class ConsoleHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IHostApplicationLifetime _appLifetime;
+
+        public ConsoleHostedService(
+            ILogger<ConsoleHostedService> logger,
+            IHostApplicationLifetime appLifetime
+           )
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+
+            _appLifetime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        _logger.LogInformation("ConsoleWithDependencyInjection - Start");
+
+                        
+                        _logger.LogInformation("ConsoleWithDependencyInjection - Job is done.");
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Unhandled exception!");
+                    }
+                    finally
+                    {
+                        // Stop the application once the work is done
+                        _appLifetime.StopApplication();
+                    }
+                });
+            });
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("ConsoleWithDependencyInjection - stopping ...");
+            return Task.CompletedTask;
+        }
+    }
+}
--- ConsoleWithDependencyInjection/Program.cs
+++ ConsoleWithDependencyInjection/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ConsoleWithDependencyInjection
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleWithDependencyInjection
 {
+    /// <summary>
+    /// Old school. No top level statements
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Standard Main entry point
+        /// </summary>
+        /// <see cref="https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/program-structure/main-command-line"/>
+        /// <param name="args">Command line argument</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IHost host = Host.CreateDefaultBuilder(args)
+               .ConfigureHostConfiguration(hostConfiguration =>
+               {
+                   hostConfiguration.SetBasePath(Directory.GetCurrentDirectory());
+                   hostConfiguration.AddJsonFile("appsettings.json");
+                   hostConfiguration.AddCommandLine(args);
+               })
+
+               .ConfigureServices((hostContext, services) =>
+               {
+                   services.AddHostedService<ConsoleHostedService>();
+
+               }).Build();
+
+            host.Run();
         }
     }
 }
