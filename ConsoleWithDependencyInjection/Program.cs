d31a7ba64d4ff1de9a9aee725be1b4ca25f2913b
Milan Křepelka <milan.krepelka@assecosol.com>
2023-08-28 15:50:30 +0200

    Add project files.

---
+++ ConsoleWithDependencyInjection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleWithDependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
